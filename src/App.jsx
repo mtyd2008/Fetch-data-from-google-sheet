c48fa4d99feb9cb7dd062362ad50d91e6b50ed95
mtyd2008 <tahaydandya@gmail.com>
2025-04-22 15:26:31 +0500

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,47 +8,59 @@ const App = () => {
   const SHEET_ID = "1_m1r4uqhlNMosyy5GQBg0XgqqKGCqfJWyydwnIARAxg";
   const RANGE = "B3:D50";
   const API_KEY = "AIzaSyAujRd62kDheKBWAGbydc1YglJE6KDD8v8";
-  const api =  `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`
-
+  
   useEffect(() => {
+    const api =  `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`
     axios.get(api)
       .then(response => {
-        console.log(response.data.values);
-        setData(response.data.values || []);
+        const res = response.data.values
+        setData(res);
       })
       .catch(err => {
         console.error("Error fetching data:", err);
       });
   }, []);
 
-// console.log(data);
+console.log(data);
 
 
-  return (
-    <>
-    <h1 className='text-center text-3xl my-3'>Fetch data from Google sheet</h1>
-    
-    <div className='flex justify-center items-center flex-wrap'>
-    {data.map((product , index)=>{
-      <div className="card bg-base-100 w-96 shadow-sm" key={index}>
+return (
+  <>
+  <div style={{
+    textAlign:"center",
+    backgroundColor:'black',
+    color:'white'
+  }}>
+  <h1>Fetch data from Google sheet</h1>
+  </div>
+  
+  <div style={{
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    flexWrap:'wrap',
+    gap:'40px',
+  }}>
+  {data.map((product , index)=>(
+    <div key={index} style={{
+      border:'1px solid black',
+      padding:'15px',
+      borderRadius:'15px'
+    }}>
       <figure>
-        <img
-          src={product[1]}
-          alt={product[0]} />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{product[0]}</h2>
-        <p>{product[2]}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
+      <img width={'250px'} src={product[1]} alt={product[0]} />
+    </figure>
+    <div>
+      <h3>{product[0]}</h3>
+      <p><b>Price: Rs</b> {product[2]}</p>
     </div>
-    })}
     </div>
-    
-    </>
-  )
+
+  ))}
+  </div>
+  
+  </>
+)
 }
 
-export default App
+export default App 
